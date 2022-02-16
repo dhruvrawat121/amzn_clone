d4c414dbaa461aebe860d43c9a68a6a0eccdb74e
dhruvrawat121 <79772806+dhruvrawat121@users.noreply.github.com>
2022-02-16 22:15:17 +0530

    carousel

--- components/Banner.js
+++ components/Banner.js
@@ -4,18 +4,18 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 
 export default function Banner() {
   return (
-      <div className="relative">
-          <div className="absolute w-full h-32 bg-gradient-to-t from-red-100 to-tranparent bottom-0 z-10"/>
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-red-100 to-tranparent bottom-0 z-10" />
       <Carousel
-        autoplay
+        autoPlay
         infiniteLoop
         showStatus={false}
         showIndicators={false}
         showThumbs={false}
-        interval={2000}
+        swipeable={true}
       >
         <div>
-                  <img src="https://links.papareact.com/gi1" alt="" loading="lazy" />
+          <img src="https://links.papareact.com/gi1" alt="" loading="lazy" />
         </div>
         <div>
           <img src="https://links.papareact.com/6ff" alt="" loading="lazy" />
